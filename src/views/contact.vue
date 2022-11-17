62d6de811a7ab33e9a49afc684cf5a1a93fe8a0d
wangoimwangi <68998392+wangoimwangi@users.noreply.github.com>
2022-11-17 18:41:03 +0300

    First Commit

---
+++ src/views/contact.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="contact">
+        <img alt="Our Logo" src="../assets/logo.png">
+        <div id="contact">
+            <h2>Contact us</h2>
+            <p>Contact us by phone on {{ phoneNumber }}, or via our address at 123 Fake Street,
+                New York City.</p>
+        </div>
+        <footer>
+        </footer>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Contact',
+    data() {
+        return {
+            phoneNumber: '123-123-123'
+        }
+    }
+}
+</script>
+<style scoped="">
+#contact {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu,
+        Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    padding: 2rem;
+    margin: 2rem;
+    border: 1px solid #ddd;
+    border-radius: 10px;
+}
+
+h2 {
+    margin: 0 0 1.5rem 0;
+}
+</style>
